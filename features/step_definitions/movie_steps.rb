cde4eda624b1c4e5b134b10060e684edfb85a057
Milindu Sanoj Kumarage <agentmilindu@gmail.com>
2012-10-27 16:40:43 +0530

    HW3 part 2.a

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -22,6 +22,19 @@ end
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
+  ratings=rating_list.split(",")
+  if(unckeck!="un"){
+  ratings.each do|rating|
+    step "when I check #{rating}"
+  end
+  }
+  else{
+  ratings.each do|rating|
+    step "when I check #{rating}"
+  end
+  }
+  
+  
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
 end
